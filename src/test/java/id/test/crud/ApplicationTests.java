9e058e766059ae82762bbc2fdc83c635af980ec1
Denny Afrizal <denny.afrizal@alfadigital.id>
2020-07-07 16:25:04 +0700

    Project Skeleton

---
+++ src/test/java/id/test/crud/ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.test.crud;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
